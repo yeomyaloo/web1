efb8d6f028677423476a225ea6a59f2fd8089379
hwiju yeom <yeom456@gmail.com>
2021-09-19 15:09:10 +0900

    9/19

---
+++ URINURI/URINURI/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import sys
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+    
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug = True)
